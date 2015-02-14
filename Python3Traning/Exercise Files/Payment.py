ad85f1b61072a3478982f889664c4e8e8727d8cb
Daniel <leonik.daniel@gmail.com>
2015-02-14 15:03:41 +0100

    First

---
+++ Python3Traning/Exercise Files/Payment.py
@@ -0,0 +1,6 @@
+inp = input('Enter hours: ')
+hours = float(inp)
+inp = input ('Enter rate: ')
+rate = float(inp)
+pay = hours * rate
+print (pay) 
---
+++ Python3Traning/Exercise Files/hello.py
@@ -0,0 +1,3 @@
+#print first dialog.
+
+print('Hello world')
